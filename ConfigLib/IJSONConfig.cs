9a773f1bc4a536c7e93de40c1554870132e087de
Amr Swalha <amr.swalha.code@outlook.com>
2018-01-19 22:28:45 +0200

    Renhancing work to much better architecture

---
+++ ConfigLib/IJSONConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonConfiguration
+{
+    public interface IJsonConfiguration
+    {
+        void ConfigurationSource();
+    }
+
+    public interface IConfigurationSource
+    {
+        void FileSource(string pathToFile);
+    }
+
+    class ConfigurationSource : IConfigurationSource
+    {
+        /// <summary>
+        /// Set the source of the configuration as file path
+        /// </summary>
+        /// <param name="pathToFile">The json file location</param>
+        public void FileSource(string pathToFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
