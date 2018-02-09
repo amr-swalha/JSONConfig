37630897d56f80156d8fcb5c5bc011e5b073613c
Amr Swalha <amr.swalha.code@outlook.com>
2018-02-09 23:20:04 +0200

    More enhancements

--- ConfigLib/IJSONConfig.cs
+++ ConfigLib/IJSONConfig.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
 
 namespace JsonConfiguration
 {
@@ -13,28 +12,107 @@ namespace JsonConfiguration
 
     public interface IConfigurationSource
     {
-        void FileSource(string pathToFile);
-        void WebSource(string pathToFile);
-        void DbSource(string pathToFile);
+        IJsonConfigurationData FileSource(string pathToFile);
+        IJsonConfigurationData WebSource(string urlToFile);
+        SqlConnection DbSource(string connectionString);
     }
 
-    class ConfigurationSource : IConfigurationSource
+    public interface IJsonConfigurationData
     {
-        public void DbSource(string connectionString)
+        int Count { get; set; }
+        Dictionary<string, string> Configs { get; set; }
+        Exception Error { get; set; }
+        bool IsSuccessed { get; set; }
+    }
+
+    public interface IJsonConfigurationAccess
+    {
+        IJsonConfigurationData GetKey(string key);
+        IJsonConfigurationData AddKey(string key, object value);
+    }
+
+    public interface IJsonConfigurationDatabaseSource
+    {
+
+    }
+    public class JsonConfigurationAccess : IJsonConfigurationAccess
+    {
+        public IConfigurationSource ConfigurationSources { get; set; }   
+        public JsonConfigurationAccess(IConfigurationSource source)
+        {
+            ConfigurationSources = source;
+        }
+        public IJsonConfigurationData AddKey(string key, object value)
         {
             throw new NotImplementedException();
+        }
+
+        public IJsonConfigurationData GetKey(string key)
+        {
+            var data = new JsonConfigurationData();
+         
+
+            return null;
+        }
+    }
+    public class JsonConfigurationData : IJsonConfigurationData
+    {
+        public int Count { get; set; }
+        public Dictionary<string, string> Configs { get; set; }
+        public Exception Error { get; set; }
+        public bool IsSuccessed { get; set; }
+    }
+    public class ConfigurationSource : IConfigurationSource
+    {
+        private static volatile ConfigurationSource _instance;
+        private static object syncRoot = new object();
+        public static ConfigurationSource Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (_instance == null)
+                            _instance = new ConfigurationSource();
+                    }
+                }
+                return _instance;
+            }
+        }
+        private ConfigurationSource()
+        {
+
+        }
+
+        public SqlConnection DbSource(string connectionString)
+        {
+            var config = new JsonConfigurationData();
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                return connection;
+            }
+            catch (Exception e)
+            {
+                config.Error = e;
+            }
+
+            return null;
         }
 
         /// <summary>
         /// Set the source of the configuration as file path
         /// </summary>
         /// <param name="pathToFile">The json file location</param>
-        public void FileSource(string pathToFile)
+        public IJsonConfigurationData FileSource(string pathToFile)
         {
+
             throw new NotImplementedException();
         }
 
-        public void WebSource(string url)
+        public IJsonConfigurationData WebSource(string url)
         {
             throw new NotImplementedException();
         }
